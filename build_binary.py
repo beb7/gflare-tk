2f021131622f5261303bd5359d857fe6839c6492
Benjamin Goerler <ben@lavabyte.net>
2020-05-09 14:38:16 +0100

    Fix icon path

--- build_binary.py
+++ build_binary.py
@@ -2,7 +2,7 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"include_files": ["greenflare-icon-64x64.png", "greenflare-icon-256x256.ico"]}
+build_exe_options = {"include_files": ["icons/greenflare-icon-64x64.png", "icons/greenflare-icon-256x256.ico"]}
 
 
 
@@ -26,10 +26,10 @@ shortcut_table = [
 msi_data = {"Shortcut": shortcut_table}
 
 # Change some default MSI options and specify the use of the above defined tables
-bdist_msi_options = {'data': msi_data, 'install_icon': "greenflare-icon-256x256.ico"}
+bdist_msi_options = {'data': msi_data, 'install_icon': "icons/greenflare-icon-256x256.ico"}
 
 # MacOS
-bdist_mac_options = {'iconfile': 'greenflare-icon-64x64.icns', 'bundle_name': 'greenflare'}
+bdist_mac_options = {'iconfile': 'icons/greenflare-icon-64x64.icns', 'bundle_name': 'greenflare'}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
@@ -42,14 +42,14 @@ else:
 
 
 target = Executable(
-    script="gflare-gui.py",
+    script="greenflare.py",
     base=base,
-    icon="greenflare-icon-256x256.ico",
+    icon="icons/greenflare-icon-256x256.ico",
     targetName=targetName
     )
 
 setup(  name = "Greenflare SEO Crawler",
-        version = "0.63",
+        version = "0.64",
         author = "Geenflare",
         description = "Scalable, low memory SEO crawler",
         options = {"build_exe": build_exe_options, "bdist_msi": bdist_msi_options, "bdist_mac": bdist_mac_options},
