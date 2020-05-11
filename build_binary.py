a3e937fad6aa33cf8183430e519dfdb6b8544498
Benjamin Goerler <ben@lavabyte.net>
2020-05-11 09:19:52 +0100

    Add application shortcut to dmg

--- build_binary.py
+++ build_binary.py
@@ -29,7 +29,7 @@ msi_data = {"Shortcut": shortcut_table}
 bdist_msi_options = {'data': msi_data, 'install_icon': "icons/greenflare-icon-256x256.ico"}
 
 # MacOS
-bdist_mac_options = {'iconfile': 'icons/greenflare-icon-64x64.icns', 'bundle_name': 'greenflare'}
+bdist_mac_options = {'iconfile': 'icons/greenflare-icon-64x64.icns', 'bundle_name': 'greenflare', 'applications_shortcut': True}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
