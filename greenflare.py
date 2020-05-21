9cb83e57b916acab503b2232806be8f69d053891
Benjamin Goerler <ben@lavabyte.net>
2020-05-21 15:57:39 +0100

    Move menu call to mainWindow

--- greenflare.py
+++ greenflare.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import Frame, ttk
+from tkinter import Frame, ttk, Menu
 from core.gflarecrawler import GFlareCrawler
 from widgets.crawltab import CrawlTab
 from threading import Lock
@@ -17,6 +17,41 @@ class mainWindow(Frame):
 		self.tab_parent.add(self.tab_crawl, text="Crawl")
 		self.tab_parent.pack(expand=1, fill="both")
 		self.master.title("Greenflare SEO Crawler")
+
+		self.menubar = Menu(self)
+
+		self.filemenu = Menu(self.menubar, tearoff=0)
+		self.filemenu.add_command(label="Load Crawl", command=self.load_crawl)
+		self.filemenu.add_separator()
+		self.filemenu.add_command(label="Full Export", command=self.full_export)
+		self.menubar.add_cascade(label="File", menu=self.filemenu)
+
+		self.modemenu = Menu(self.menubar, tearoff=0)
+		self.modemenu.add_command(label="Spider", command=self.spider_mode)
+		self.modemenu.add_command(label="List", command=self.list_mode)
+		self.menubar.add_cascade(label="Mode", menu=self.modemenu)
+
+		self.aboutmenu = Menu(self.menubar, tearoff=0)
+		self.aboutmenu.add_command(label="About", command=self.show_about)
+		self.menubar.add_cascade(label="Help", menu=self.aboutmenu)
+
+		root.config(menu=self.menubar)
+
+
+	def load_crawl(self):
+		pass
+
+	def full_export(self):
+		pass
+
+	def spider_mode(self):
+		pass
+
+	def list_mode(self):
+		pass
+
+	def show_about(self):
+		pass
 		
 if __name__ == "__main__":
 	if getattr(sys, 'frozen', False): WorkingDir = path.dirname(sys.executable)
@@ -25,6 +60,7 @@ if __name__ == "__main__":
 	root = tk.Tk()
 	root.geometry("1024x768")
 	root.iconphoto(False, tk.PhotoImage(file=WorkingDir + path.sep + 'greenflare-icon-64x64.png'))
+
 	globalLock = Lock()
 	columns = [("url", "TEXT type UNIQUE"), ("content_type" , "TEXT"), ("status_code", "INT"), ("indexability", "TEXT"), ("h1", "TEXT"), ("h2", "TEXT"), ("page_title", "TEXT"), ("meta_description", "TEXT"), ("canonical_tag", "TEXT"), ("robots_txt", "TEXT"), ("redirect_url", "TEXT"), ("meta_robots", "TEXT"), ("x_robots_tag", "TEXT"), ("unique_inlinks", "INT")]
 	Settings  = {"MODE": "Spider", "THREADS": 5, "URLS_PER_SECOND": 15, "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36", "UA_SHORT": "Windows Chrome", "MAX_RETRIES": 3, "CRAWL_ITEMS": columns}
