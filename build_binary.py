ca735a3062e71b1823cd4c30dcf35069bc841031
Benjamin Goerler <ben@lavabyte.net>
2020-07-24 19:28:03 +0100

    Try something clever again

--- build_binary.py
+++ build_binary.py
@@ -28,8 +28,8 @@ msi_data = {"Shortcut": shortcut_table}
 bdist_msi_options = {'data': msi_data, 'install_icon': "greenflare-icon-256x256.ico"}
 
 # MacOS
-# bdist_mac_options = {'iconfile': 'greenflare-icon-64x64.icns', 'bundle_name': 'greenflare', 'custom_info_plist': 'Info.plist', 'include_resources': [('/usr/local/Cellar/tcl-tk/8.6.10/lib/tcl8.6', 'tcl8.6'), ('/usr/local/Cellar/tcl-tk/8.6.10/lib/tk8.6', 'tk8.6')]}
-bdist_mac_options = {'iconfile': 'greenflare-icon-64x64.icns', 'bundle_name': 'greenflare', 'custom_info_plist': 'Info.plist'}
+bdist_mac_options = {'iconfile': 'greenflare-icon-64x64.icns', 'bundle_name': 'greenflare', 'custom_info_plist': 'Info.plist', 'include_resources': [('/usr/local/Cellar/tcl-tk/8.6.10/lib/tcl8.6', 'tcl8.6'), ('/usr/local/Cellar/tcl-tk/8.6.10/lib/tk8.6', 'tk8.6')]}
+# bdist_mac_options = {'iconfile': 'greenflare-icon-64x64.icns', 'bundle_name': 'greenflare', 'custom_info_plist': 'Info.plist'}
 bdist_dmg_options = {'applications_shortcut': True, "volume_label": "Greenflare SEO Crawler"}
 
 # GUI applications require a different base on Windows (the default is for a
