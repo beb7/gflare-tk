12d5f3f27290b0eb308996a6ce20a9a1692642d6
Benjamin Goerler <ben@lavabyte.net>
2020-07-24 11:23:47 +0100

    do not include tkinter on mac

--- build_binary.py
+++ build_binary.py
@@ -28,8 +28,8 @@ msi_data = {"Shortcut": shortcut_table}
 bdist_msi_options = {'data': msi_data, 'install_icon': "greenflare-icon-256x256.ico"}
 
 # MacOS
-# bdist_mac_options = {'iconfile': 'greenflare-icon-64x64.icns', 'bundle_name': 'greenflare', 'codesign_identity': 'ben@lavabyte.net', 'codesign_deep': True, 'custom_info_plist': 'Info.plist'}
-bdist_mac_options = {'iconfile': 'greenflare-icon-64x64.icns', 'bundle_name': 'greenflare', 'custom_info_plist': 'Info.plist', 'include_resources': [('/usr/local/Cellar/tcl-tk/8.6.10/lib/tcl8.6', 'tcl8.6'), ('/usr/local/Cellar/tcl-tk/8.6.10/lib/tk8.6', 'tk8.6')]}
+# bdist_mac_options = {'iconfile': 'greenflare-icon-64x64.icns', 'bundle_name': 'greenflare', 'custom_info_plist': 'Info.plist', 'include_resources': [('/usr/local/Cellar/tcl-tk/8.6.10/lib/tcl8.6', 'tcl8.6'), ('/usr/local/Cellar/tcl-tk/8.6.10/lib/tk8.6', 'tk8.6')]}
+bdist_mac_options = {'iconfile': 'greenflare-icon-64x64.icns', 'bundle_name': 'greenflare', 'custom_info_plist': 'Info.plist'}
 bdist_dmg_options = {'applications_shortcut': True, "volume_label": "Greenflare SEO Crawler"}
 
 # GUI applications require a different base on Windows (the default is for a
@@ -41,9 +41,11 @@ if sys.platform == "win32":
 	# base = "Win32GUI"
 	targetName="greenflare.exe"
 elif sys.platform == "darwin":
-    print("Applying tcl-tk hack ...")
-    environ['TK_LIBRARY'] = '/usr/local/Cellar/tcl-tk/8.6.10/lib/tk8.6/'
-    environ['TCL_LIBRARY'] = '/usr/local/Cellar/tcl-tk/8.6.10/lib/tcl8.6/' 
+    print("Exclude tkinter on macos and rely on an external install ...")
+    build_exe_options["exclude": ["tkinter"]]
+#     print("Applying tcl-tk hack ...")
+#     environ['TK_LIBRARY'] = '/usr/local/Cellar/tcl-tk/8.6.10/lib/tk8.6/'
+#     environ['TCL_LIBRARY'] = '/usr/local/Cellar/tcl-tk/8.6.10/lib/tcl8.6/' 
 
 target = Executable(
     script="greenflare.py",
