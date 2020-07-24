123f746eaaeef2f3b9a3ace64f3dc972cc0d85b2
Benjamin Goerler <ben@lavabyte.net>
2020-07-24 19:32:27 +0100

    fix path joining

--- greenflare.py
+++ greenflare.py
@@ -93,8 +93,8 @@ if __name__ == "__main__":
 	if getattr(sys, 'frozen', False): 
 		WorkingDir = path.dirname(sys.executable)
 		if sys.platform == "darwin":
-			environ['TK_LIBRARY'] = Path(WorkingDir).parent + '/Resources/tk8.6/'
-			environ['TCL_LIBRARY'] = Path(WorkingDir).parent + '/Resources/tcl8.6/'
+			environ['TK_LIBRARY'] = str(Path(WorkingDir).parent) + '/Resources/tk8.6/'
+			environ['TCL_LIBRARY'] = str(Path(WorkingDir).parent) + '/Resources/tcl8.6/'
 			print(environ['TCL_LIBRARY'])
 			print(environ['TK_LIBRARY'])
 	else:
