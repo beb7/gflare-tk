34e59037520887cfc2fa9d9d81b4fd04b756328e
Benjamin Goerler <ben@lavabyte.net>
2020-09-28 08:43:43 +0100

    win: Use new icon and switch to binary builds only (no installer)

--- build_binary.py
+++ build_binary.py
@@ -3,7 +3,7 @@ from cx_Freeze import setup, Executable
 from os import environ
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"include_files": ["greenflare-icon-64x64.png", "greenflare-icon-256x256.ico"]}
+build_exe_options = {"include_files": ["greenflare-icon-32x32.png"]}
 
 shortcut_table = [
     ("DesktopShortcut",        # Shortcut
@@ -25,7 +25,7 @@ shortcut_table = [
 msi_data = {"Shortcut": shortcut_table}
 
 # Change some default MSI options and specify the use of the above defined tables
-bdist_msi_options = {'data': msi_data, 'install_icon': "greenflare-icon-256x256.ico"}
+bdist_msi_options = {'data': msi_data, 'install_icon': "greenflare-icon-32x32.ico"}
 
 # MacOS
 bdist_mac_options = {'iconfile': 'greenflare-icon-64x64.icns', 'bundle_name': 'greenflare', 'custom_info_plist': 'Info.plist', 'include_resources': [('/usr/local/Cellar/tcl-tk/8.6.10/lib/tcl8.6', 'tcl8.6'), ('/usr/local/Cellar/tcl-tk/8.6.10/lib/tk8.6', 'tk8.6')]}
@@ -38,7 +38,7 @@ base = None
 
 targetName="greenflare"
 if sys.platform == "win32":
-	# base = "Win32GUI"
+	base = "Win32GUI"
 	targetName="greenflare.exe"
 # elif sys.platform == "darwin":
 #     print("Applying tcl-tk hack ...")
@@ -48,13 +48,13 @@ if sys.platform == "win32":
 target = Executable(
     script="greenflare.py",
     base=base,
-    icon="greenflare-icon-256x256.ico",
+    icon="greenflare-icon-32x32.ico",
     targetName=targetName
     )
 
 setup(  name = "Greenflare SEO Crawler",
-        version = "0.64",
+        version = "0.75",
         author = "Geenflare",
-        description = "Scalable, low memory SEO crawler",
+        description = "Greenflare SEO Crawler",
         options = {"build_exe": build_exe_options, "bdist_msi": bdist_msi_options, "bdist_mac": bdist_mac_options, "bdist_dmg": bdist_dmg_options},
         executables = [target])
