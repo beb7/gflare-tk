5dab6e6421865a7582166a211f78ada0789f2de8
Benjamin Goerler <ben@lavabyte.net>
2020-10-06 13:42:58 +0100

    Release v0.78

--- setup-py2app.py
+++ setup-py2app.py
@@ -15,7 +15,7 @@ OPTIONS = {
 		'CFBundleName': 'Greenflare',
 		'CFBundleIconFile': 'greenflare.icns',
 		'CFBundleIdentifier': 'io.greenflare.crawler',
-		'CFBundleShortVersionString': '0.77.4',
+		'CFBundleShortVersionString': '0.78',
 		'CFBundleDocumentTypes': [{'CFBundleTypeExtensions': ["gflaredb"], 'CFBundleTypeName': "Greenflare Crawl DB", 'CFBundleTypeRole': "Editor"}]
 		},
 	'argv_emulation': False
