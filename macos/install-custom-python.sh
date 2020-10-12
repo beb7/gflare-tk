259190f5f5f8c25b2413b4b2aab4fc91443bafea
Benjamin Goerler <ben@lavabyte.net>
2020-10-12 13:30:01 +0100

    macOS: install sphinx-doc

--- macos/install-custom-python.sh
+++ macos/install-custom-python.sh
@@ -30,7 +30,7 @@ if [ ! -d "$BUILD_DIR" ]; then
 	sed -i.bak 's#"tk868_on_10_8_10_9.patch",##g' build-installer.py
 	mv build-installer.py.bak build-installer.py
 
-	brew install sphinx
+	brew install sphinx-doc
 	echo "Add sphinx to PATH"
 	sed -i.bak "s,'/bin:/sbin:/usr/bin:/usr/sbin','/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/opt/sphinx-doc/bin',g" build-installer.py
 	sed -i.bak "s,'/bin:/sbin:/usr/bin:/usr/sbin','/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/opt/sphinx-doc/bin',g" build-installer.py
