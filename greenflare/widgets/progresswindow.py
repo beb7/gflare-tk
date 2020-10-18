6ba416852f143fc0795ef639fb8b5419ab78111e
Benjamin Görler <ben@lavabyte.net>
2020-10-18 23:58:45 +0200

    Center progress windows a little bit better

--- greenflare/widgets/progresswindow.py
+++ greenflare/widgets/progresswindow.py
@@ -1,10 +1,10 @@
 from tkinter import ttk, Toplevel, BOTH, TOP
 
-class ProgressWindow(ttk.Frame):    
+class ProgressWindow(ttk.Frame):
 	def __init__(self, title=None, msg=None):
 		ttk.Frame.__init__(self)
 		self.window = Toplevel(self.master)
-		
+
 		self.topframe = ttk.Frame(self.window)
 		self.topframe.pack(anchor='center', padx=20, pady=20, fill="x")
 
@@ -20,21 +20,17 @@ class ProgressWindow(ttk.Frame):
 		# The window needs to be placed after its elements have been assigned
 		# get window width and height
 
-		# this always returns 200, so setting the window size manually at the moment ...
-		# w = self.window.winfo_reqwidth()
-		# h = self.window.winfo_reqheight()
+		height = self.master.winfo_height()
+		width = self.master.winfo_width()
 
-		w = 300
-		h = 150
+		pop_up_height = self.window.winfo_height()
+		pop_up_width = self.window.winfo_width()
 
-		# get screen width and height
-		ws = self.master.winfo_screenwidth() # width of the screen
-		hs = self.master.winfo_screenheight() # height of the screen
+		x = self.master.winfo_rootx()
+		y = self.master.winfo_rooty()
 
-		# calculate x and y coordinates for the Tk root window
-		x = (ws/2) - (w/2)
-		y = (hs/2) - (h/2)
+		x_offset = width // 2 - pop_up_width
+		y_offset = height // 2 - pop_up_height
 
-		# set the dimensions of the screen 
 		# and where it is placed
-		self.window.geometry('%dx%d+%d+%d' % (w, h, x, y))
+		self.window.geometry('+%d+%d' % (x+x_offset, y+y_offset))
