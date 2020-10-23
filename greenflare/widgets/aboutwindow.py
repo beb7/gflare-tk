ac4637fd864728fbf31bb93201b68c0a596f66d8
Benjamin Goerler <ben@lavabyte.net>
2020-10-23 11:15:21 +0100

    Add About Window

---
+++ greenflare/widgets/aboutwindow.py
@@ -0,0 +1,55 @@
+from tkinter import ttk, Toplevel, TOP, LEFT, Text, END
+from PIL import ImageTk, Image
+
+class AboutWindow(Toplevel):
+	def __init__(self):
+		Toplevel.__init__(self)
+		
+		self.resizable(False, False)
+		
+		self.title('About Greenflare SEO Crawler')
+		
+		self.leftframe = ttk.Frame(self)
+		self.leftframe.pack(side=LEFT, padx=20, pady=20, fill="x")
+
+		self.rightframe = ttk.Frame(self)
+		self.rightframe.pack(side=LEFT, padx=20, pady=20, fill="x")
+
+		self.render = ImageTk.PhotoImage(Image.open('resources/greenflare-icon-256x256.png'))
+		self.img = ttk.Label(self.leftframe, image=self.render)
+		self.img.pack(padx=20, pady=20)
+
+		self.info_text = Text(self)
+		self.info_text.pack(padx=20, pady=20)
+
+		self.info_text.tag_configure('h1', font=('Arial', 16, 'bold'))
+		self.info_text.tag_configure('h2', font=('Arial', 14))
+
+		# The window needs to be placed after its elements have been assigned
+		# get window width and height
+
+		height = self.master.winfo_height()
+		width = self.master.winfo_width()
+
+		pop_up_height = self.winfo_height()
+		pop_up_width = self.winfo_width()
+
+		x = self.master.winfo_rootx()
+		y = self.master.winfo_rooty()
+
+		x_offset = width // 2 - 2 * pop_up_width
+		y_offset = height // 2 - pop_up_height
+
+		# and where it is placed
+		self.geometry('+%d+%d' % (x+x_offset, y//2+y_offset))
+		self.lift()
+
+
+		heading_1 = 'Greenflare SEO Crawler'
+		heading_2 = '\nVersion 0.90'
+
+		text = '\n\n© Greenflare Developers 2020\n\nCreated By Benjamin Görler (ben@greenflare.io)'
+
+		self.info_text.insert(END, heading_1, 'h1')
+		self.info_text.insert(END, heading_2, 'h2')
+		self.info_text.insert(END, text)
