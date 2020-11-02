982ab42320c0501de993e2c247775625040bd8f9
Benjamin Goerler <ben@lavabyte.net>
2020-11-02 15:01:25 +0000

    Properly center window

--- greenflare/widgets/progresswindow.py
+++ greenflare/widgets/progresswindow.py
@@ -1,4 +1,5 @@
 from tkinter import ttk, Toplevel, TOP
+center_on_parent(self.master, self)
 
 
 class ProgressWindow(Toplevel):
@@ -20,21 +21,6 @@ class ProgressWindow(Toplevel):
         self.pb.pack(fill="x", pady=(0, 20), padx=20, side=TOP, expand=True)
         self.pb.start(50)
 
-        # The window needs to be placed after its elements have been assigned
-        # get window width and height
+        center_on_parent(self.master, self)
 
-        height = self.master.winfo_height()
-        width = self.master.winfo_width()
-
-        pop_up_height = self.winfo_height()
-        pop_up_width = self.winfo_width()
-
-        x = self.master.winfo_rootx()
-        y = self.master.winfo_rooty()
-
-        x_offset = width // 2 - 2 * pop_up_width
-        y_offset = height // 2 - pop_up_height
-
-        # and where it is placed
-        self.geometry('+%d+%d' % (x + x_offset, y // 2 + y_offset))
         self.lift()
