1e2791ab6fe2065bc4d2afa4a86e13807bc1b9ad
Benjamin Goerler <ben@lavabyte.net>
2020-11-11 15:54:35 +0000

    Add License

---
+++ greenflare/core/__init__.py
@@ -0,0 +1,23 @@
+"""
+@author Benjamin Görler <ben@greenflare.io>
+
+@section LICENSE
+
+Greenflare SEO Web Crawler (https://greenflare.io)
+Copyright (C) 2020  Benjamin Görler. This file is part of
+Greenflare, an open-source project dedicated to delivering
+high quality SEO insights and analysis solutions to the world.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
