e5d632d02d7491fa7f00fbfe1c779c280aa94349
beb7 <ben@lavabyte.net>
2020-12-07 13:30:20 +0100

    Add python versions 3.7 and 3.9

--- setup.py
+++ setup.py
@@ -45,7 +45,9 @@ setup(
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Internet :: WWW/HTTP',
         'Operating System :: OS Independent',
     ],
